642a00d1218d05b9afee4db969cfbb6b00ee2e94
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2025-04-09 17:48:38 +0100

    feat: add fibs and fibsRec

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,36 @@
-function fib(fibCount) {
-    return fibCount;
+function fibs(fibCount = 2) {
+  let fibArr = [0, 1];
+
+  if (fibCount < 2) {
+    return "Fib sequence cannot be less than 2.";
+  } else {
+    fibCount -= 2;
+    for (let i = 0; i < fibCount; i++) {
+      let lastNumber = fibArr[fibArr.length - 1];
+      let nxtToLastNumber = fibArr[fibArr.length - 2];
+      fibArr.push(lastNumber + nxtToLastNumber);
+    }
+  }
+
+  return fibArr;
 }
 
-function fibRec(fibCount) {
-    return fibCount;
+console.log(fibs(13));
+console.log(fibsRec(13));
+
+function fibsRec(fibCount) {
+  if (fibCount <= 0) {
+    return [];
+  } else if (fibCount === 1) {
+    return [0];
+  } else if (fibCount === 2) {
+    return [0, 1];
+  } else {
+    let prevSequence = fibsRec(fibCount - 1);
+    let prevLength = prevSequence.length;
+    let nextSequence =
+      prevSequence[prevLength - 1] + prevSequence[prevLength - 2];
+    prevSequence.push(nextSequence);
+    return prevSequence;
+  }
 }
